509ef11c33e16db5c1d310a02ea8651bbd176781
Alexander Agafonov <asasha2017@yandex.ru>
2022-04-13 00:19:51 +0300

    delete extra files

--- task2/A/index.h
+++
@@ -1,2 +0,0 @@
-#pragma once
- int GetOne() { return 1; }
--- task2/B/CMakeFiles/myLib.dir/lib.cpp.o.d
+++
@@ -1,3 +0,0 @@
-B/CMakeFiles/myLib.dir/lib.cpp.o: \
-  /Users/aleksandragafonov/git/github/TPtask2/task2/B/lib.cpp \
-  /Users/aleksandragafonov/git/github/TPtask2/task2/B/lib.h
