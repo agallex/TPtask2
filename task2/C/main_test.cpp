de47c090ac94a3f2b4a65b82ec2e097f1d00bc94
agallex <63646415+agallex@users.noreply.github.com>
2022-05-18 15:33:10 +0300

    Исправил количество тестов.

--- task2/C/main_test.cpp
+++ task2/C/main_test.cpp
@@ -2,25 +2,16 @@
 #include "A/index.h"
 #include "B/lib.h"
 
-TEST_F(MainTest, Addition) {
+TEST_F(MainTest, StandardOperations) {
     ASSERT_EQ(add(3, 5), 8);
     ASSERT_EQ(add(3, -7), -4);
     ASSERT_EQ(add(-5, 5), 0);
-}
-
-TEST_F(MainTest, Subtraction) {
     ASSERT_EQ(sub(3, 5), -2);
     ASSERT_EQ(sub(3, -7), 10);
     ASSERT_EQ(sub(-5, 5), -10);
-}
-
-TEST_F(MainTest, Multiplication) {
     ASSERT_EQ(mul(3, 5), 15);
     ASSERT_EQ(mul(3, -7), -21);
     ASSERT_EQ(mul(-5, 5), -25);
-}
-
-TEST_F(MainTest, Division) {
     ASSERT_EQ(divis(10, 5), 2);
     ASSERT_EQ(divis(10, 3), 3);
     ASSERT_EQ(divis(10, 7), 1);
